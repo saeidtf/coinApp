8018f9d36dc77f4f0fbca30d5ebcd25aabfb1d10
Saeid Taheri Fard <saeidtf@gmail.com>
2023-02-05 18:35:35 +0330

    feat: change pagination and search

--- src/domains/Home/Home.tsx
+++ src/domains/Home/Home.tsx
@@ -1,17 +1,19 @@
-import { useGetCoinsDetailsQuery, useGetCoinsQuery } from '@/services/coinsApi'
+import { ICoinsDetails, useGetCoinsDetailsQuery, useGetCoinsQuery } from '@/services/coinsApi'
 import {
   Avatar,
   Box,
   Pagination,
   Skeleton,
+  Stack,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableRow,
   TextField,
+  Typography,
 } from '@mui/material'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const debounce = (fn: any, ms: number) => {
   let timeoutId: any
@@ -21,21 +23,35 @@ const debounce = (fn: any, ms: number) => {
   }
 }
 
-const MAX_COINS = 250
-
 export default function Home() {
   const [page, setPage] = useState(1)
   const [pageSize, setPageSize] = useState(25)
   const [search, setSearch] = useState('')
+  const [coins, setCoins] = useState<ICoinsDetails[]>([])
+  const [totalSize, setTotalSize] = useState(250)
 
-  const { data: coins } = useGetCoinsQuery()
-  const { data: coinsDetail, isFetching } = useGetCoinsDetailsQuery(
-    {
-      page : 1,
-      pageSize : 250,
-    })
+  const { data: coinsName } = useGetCoinsQuery()
+  const { data: coinsDetail, isFetching } = useGetCoinsDetailsQuery({
+    page: 1,
+    pageSize: 250,
+  })
+
+  useEffect(() => {
+    if (coinsDetail) {
+      const searchCoins = coinsDetail.filter((coin) => {
+        return coin.name.toLowerCase().includes(search.toLowerCase())
+      })
+
+      setTotalSize(searchCoins.length)
+
+      const start = (page - 1) * pageSize
+      const end = start + pageSize
+      setCoins(searchCoins.slice(start, end))
+    }
+  }, [coinsDetail, page, search, pageSize])
 
   const handleSearch = debounce((e: any) => {
+    setPage(1)
     setSearch(e.target.value)
   }, 500)
 
@@ -49,28 +65,32 @@ export default function Home() {
       <Table>
         <TableHead>
           <TableRow>
-            <TableCell>Rank</TableCell>
-            <TableCell>Icon</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Price ($)</TableCell>
-            <TableCell>Market Cap</TableCell>
-            <TableCell>Volume</TableCell>
-            <TableCell>Change</TableCell>
+            <TableCell>#</TableCell>
+            <TableCell>COIN</TableCell>
+            <TableCell>PRICE ($)</TableCell>
+            <TableCell>MARKET CAP</TableCell>
+            <TableCell>TOTAL VOLUME</TableCell>
+            <TableCell>CIRCULATING SUPPLY</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {coinsDetail?.map((coin) => {
+          {coins?.map((coin) => {
             return (
               <TableRow key={coin.id}>
                 <TableCell>{coin.market_cap_rank}</TableCell>
                 <TableCell>
-                  <Avatar src={coin.image} alt={coin.name} />
+                  <Stack spacing={4} alignItems="center" direction="row">
+                    <Avatar src={coin.image} alt={coin.name} sx={{ width: 24, height: 24 }} />
+                    <Stack spacing={1}>
+                      <Typography variant="subtitle1">{coin.name}</Typography>
+                      <Typography variant="subtitle2">{coin.symbol}</Typography>
+                    </Stack>
+                  </Stack>
                 </TableCell>
-                <TableCell>{coin.name}</TableCell>
                 <TableCell>{coin.current_price.toLocaleString()}</TableCell>
                 <TableCell>{coin.market_cap.toLocaleString()}</TableCell>
                 <TableCell>{coin.total_volume.toLocaleString()}</TableCell>
-                <TableCell>{coin.price_change_24h.toLocaleString()}</TableCell>
+                <TableCell>{`${coin.circulating_supply} ${coin.symbol.toUpperCase()}`}</TableCell>
               </TableRow>
             )
           })}
@@ -78,7 +98,7 @@ export default function Home() {
       </Table>
       <Box my={2}>
         <Pagination
-          count={Math.ceil(MAX_COINS / pageSize)}
+          count={Math.ceil(totalSize / pageSize)}
           page={page}
           onChange={(e, page) => setPage(page)}
           color="primary"
